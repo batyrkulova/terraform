69911dcc9c7fccbfd47edf5ba373da3646804677
elsu loda <elsubektemirova@gmail.com>
2025-09-30 18:30:58 -0500

    Added ec2 instance

--- hometasks/session-7hw/modules/ec2-sg/main.tf
+++ hometasks/session-7hw/modules/ec2-sg/main.tf
@@ -1,4 +1,31 @@
-resource "aws_security_group" "main" {
-  name = var.name
-  description = var.description
+resource "aws_security_group" "this" {
+  name        = var.name
+  description = "Security group for web traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_cidr_blocks
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_cidr_blocks
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
 }
--- hometasks/session-7hw/modules/ec2-sg/variables.tf
+++ hometasks/session-7hw/modules/ec2-sg/variables.tf
@@ -4,3 +4,25 @@ variable "name" {
 variable "description" {
   type = string 
 }
+
+variable "name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "ingress_cidr_blocks" {
+  description = "List of CIDR blocks allowed to access ports 80 and 443"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  description = "Tags to apply to the security group"
+  type        = map(string)
+  default     = {}
+}
