b2c830dc56d365f865357c2aefb8f3aacff3523f
elsu loda <elsubektemirova@gmail.com>
2025-10-17 13:21:12 -0500

    Added new directory

---
+++ my_app@v1/sg.alb.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "alb" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "ALB-SG"
+    Environment = var.env
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "port-https" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "port-http" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
